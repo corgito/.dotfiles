a72fb9f175c07459baa8b6bbdcd19e874d58e911
corgito <YOUR_EMAIL>
2022-05-19 22:57:23 -0400

    Fixed theme and added visual improvements to org-mode.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,41 +22,54 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(auto-dark--allow-osascript t)
- '(auto-dark--dark-theme 'spacemacs-dark)
- '(auto-dark--light-theme 'spacemacs-light)
- '(custom-enabled-themes '(doom-dracula))
  '(custom-safe-themes
-   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "f8165eb2a2223c1254a1725dfe169ebd5a35deab2cae3d36e76e507034f23df6" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(hl-todo-keyword-faces
-   '(("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX+" . "#dc752f")
-     ("\\?\\?\\?+" . "#dc752f")))
+   '("1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" default))
+ '(org-agenda-custom-commands
+   '(("A" "Agenda and all upcoming appointments"
+      ((agenda "" nil)
+       (tags "APPOINTMENT" nil))
+      nil)))
  '(org-agenda-files
-   '("~/Dropbox/Projects/Pulse Analyzer/Pulse Analyzer TODO.org" "~/Dropbox/Notes/Polska.org" "~/Dropbox/Notes/personal.org" "~/Documents/Work/chn.org" "~/Documents/tweetparser.org"))
+   '("/Users/nicolaihaddal/Dropbox/Notes/CHN.org" "/Users/nicolaihaddal/Dropbox/Job Hunt/application_tracker.org" "/Users/nicolaihaddal/Documents/Work/chn.org" "/Users/nicolaihaddal/Dropbox/Notes/personal.org"))
+ '(org-capture-templates
+   '(("p" "Personal templates")
+     ("pt" "TODO entry" entry
+      (file+headline "~/Dropbox/Notes/personal.org" "Capture")
+      (file "~/Dropbox/org/tpl-todo.txt"))
+     ("pj" "Journal entry" entry
+      (file+olp+datetree "~/Dropbox/org/journal.org")
+      "* %U - %^{Activity}")
+     ("pg" "Guitar journal entry" entry
+      (file+olp+datetree "~/Dropbox/Guitar/guitar_diary.org")
+      "* %U - %^{Activity}")
+     ("w" "Work templates")
+     ("wt" "TODO entry" entry
+      (file+headline "~/Dropbox/Notes/CHN.org" "Capture")
+      (file "~/Dropbox/org/tpl-todo.txt"))
+     ("ww" "Webinar entry" entry
+      (file+headline "~/Dropbox/Notes/CHN.org" "Capture")
+      (file "~/Dropbox/org/webinar-todo.txt"))))
  '(org-export-backends '(ascii beamer html icalendar latex md odt))
  '(org-fontify-done-headline nil)
  '(org-fontify-todo-headline nil)
+ '(org-log-into-drawer t)
+ '(org-refile-allow-creating-parent-nodes 'confirm)
+ '(org-refile-targets '((org-agenda-files :tag . ":maxlevel . 2")))
+ '(org-refile-use-outline-path 'file)
  '(package-selected-packages
-   '(markdown-mode doom-modeline doom-themes ox-gfm flycheck company-quickhelp mu4e vertico olivetti zen-mode google-translate elfeed nov pdf-tools vterm auto-dark csv-mode blacken magit elpy multiple-cursors expand-region mark-multiple swiper popup-kill-ring projectile org-roam diminish spaceline company all-the-icons dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode rainbow avy smex ido-vertical-mode org-bullets spacemacs-theme beacon which-key use-package))
+   '(htmlize chocolate-theme org-modern iedit wrap-region markdown-mode doom-modeline doom-themes ox-gfm flycheck company-quickhelp mu4e vertico olivetti zen-mode google-translate elfeed nov pdf-tools vterm auto-dark csv-mode blacken magit elpy multiple-cursors expand-region mark-multiple swiper popup-kill-ring projectile org-roam diminish spaceline company all-the-icons dashboard rainbow-delimiters sudo-edit hungry-delete switch-window rainbow-mode rainbow avy smex ido-vertical-mode org-bullets spacemacs-theme beacon which-key use-package))
  '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e")))
+;;(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+;; '(default ((t (:inherit nil :extend nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 161 :width normal :foundry "nil" :family "Hack Nerd Font Mono")))))
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#292b2e" :foreground "#b2b2b2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 161 :width normal :foundry "nil" :family "Hack Nerd Font Mono")))))
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
+ )
